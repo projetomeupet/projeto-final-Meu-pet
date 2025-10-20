e195cd74553f94e965e13c95d10e5ebd51e08b88
samilly <samilleria@gmail.com>
2025-10-20 20:44:37 -0300

    teste

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Meu Pet</title>
 </head>
 <body>
-    <h1>MEU PET</h1>
+    <h1>MEU PET2</h1>
 </body>
 </html>
