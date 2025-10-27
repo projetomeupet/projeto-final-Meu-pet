46b931e96f1911338d8c506547114a3be666ddc8
samilly <samilleria@gmail.com>
2025-10-26 21:35:46 -0300

    Tela de login

--- index.php
+++ index.php
@@ -1,3 +1,5 @@
+<?php
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +8,59 @@
     <title>Meu Pet</title>
 </head>
 <body>
-    <h1>MEU PET2</h1>
+
+    <!-- Estilização da página de login -->
+    <style> 
+        body { 
+            font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(to right, rgba(139,69,19,1));
+        }
+
+        /* Estilização do formulário de login */
+            div {
+                background-color: rgba(0,0,0,0.5);
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%,-50%);
+                padding: 60px;
+                border-radius: 15px;
+                color: white;
+
+            }
+            input {
+                background-color: white;
+                padding: 15px;
+                border: none;
+                outline: none;
+                font-size: 15px;
+            }
+            button {
+                background-color: green;
+                padding: 15px;
+                width: 100%;
+                border: none;
+                border-radius: 10px;
+                color: white;
+                font-size: 15px;
+            }
+            button:hover {
+                background-color: darkgreen;
+                cursor: pointer;
+            }
+
+
+    </style>
+
+    <!-- Formulário de login -->
+    <div>
+        <h1>Login</h1> 
+        <br> </br>
+        <input type="text" placeholder="Usuário">  
+        <br> </br>
+        <input type="password" placeholder="Senha">
+        <br> <br> </br>
+        <button>Entrar</button>
+    </div>
 </body>
 </html>
