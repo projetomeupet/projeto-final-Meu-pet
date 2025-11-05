530a73269adcf70105565f988fe994935c4d1e0c
samilly <samilleria@gmail.com>
2025-11-05 16:23:49 -0300

    atualização

--- principal.php
+++ principal.php
@@ -7,5 +7,9 @@
 </head>
 <body>
   <h1>Bem-vindo ao Meu Pet!</h1>
+  <style>
+  body { font-family: Arial, Helvetica, sans-serif;
+            background-image: linear-gradient(to right, rgba(139,69,19,1));}
+            </style>
 </body>
 </html>
